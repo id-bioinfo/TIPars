d977c6e6cf26147ea9da2443917cb9d745025306
GuangchuangYu <guangchuangyu@gmail.com>
2017-01-12 18:12:44 +0800

    update

--- TIPars.java
+++ TIPars.java
@@ -296,7 +296,7 @@ public class TIPars{
 
     public static double localEstimation(int[] scores, double ABbranch) {
 	if (scores[0] + scores[1] == 0) {
-	    return 0.0;
+	    return ABbranch/3.0;
 	}
 	double d = ABbranch * ((double) scores[2])/((double) (scores[0]+scores[1]));
 	return d;
